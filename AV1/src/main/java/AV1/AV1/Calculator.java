6ed8066b6ed7a2cfb469e6503fb7f92a4578cc11
Abhilash Das <abhilashd@cybage.com>
2019-06-13 15:08:53 +0530

    Added calculator logic

---
+++ AV1/src/main/java/AV1/AV1/Calculator.java
@@ -0,0 +1,14 @@
+package AV1.AV1;
+
+public class Calculator 
+{
+	public int add(int a, int b) 
+	    {
+	        return a + b;
+	    }
+	     
+	    public int subtract(int a, int b) {
+	        return a - b;
+	    }
+}
+
