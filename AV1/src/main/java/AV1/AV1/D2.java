db2a86d0e288bc4757a0ecb17e0b15d96dbc72fc
Abhilash Das <abhilashd@cybage.com>
2019-06-13 14:41:43 +0530

    My changes

---
+++ AV1/src/main/java/AV1/AV1/D2.java
@@ -0,0 +1,10 @@
+package AV1.AV1;
+
+public class D2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "My Page 2" );
+	}
+
+}
---
+++ AV1/src/test/java/AV1/AV1/D2Test.java
@@ -0,0 +1,24 @@
+package AV1.AV1;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class D2Test extends TestCase {
+	
+	    {
+	        super( testName );
+	    }
+	    public static Test suite()
+	    {
+	        return new TestSuite( AppTest.class );
+	    }
+
+	    /**
+	     * Rigourous Test :-)
+	     */
+	    public void testApp()
+	    {
+	        assertTrue( true );
+	    }
+}
