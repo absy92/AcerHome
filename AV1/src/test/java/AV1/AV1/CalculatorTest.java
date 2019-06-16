f5cf6e63df29b8533a0dae6d872736dc3607c54d
absy92 <51427064+absy92@users.noreply.github.com>
2019-06-16 16:09:47 +0530

    Fixed the JUNIT issues

---
+++ AV1/src/test/java/AV1/AV1/CalculatorTest.java
@@ -0,0 +1,45 @@
+package AV1.AV1;
+
+import junit.framework.TestCase;
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.junit.Assert.assertThat;
+
+
+public class CalculatorTest extends TestCase 
+{
+	
+	  private Calculator classUnderTest;
+
+	  
+	  public void setUp() throws Exception {
+	    this.classUnderTest = new Calculator();
+	  }
+
+	  
+	  public void tearDown() throws Exception {
+	    classUnderTest = null;
+	  }
+
+	  
+	  public void testAdd_positiveNumbers_shouldReturnResult() {
+	    assertEquals("add", 7, classUnderTest.add(3, 4));
+	  }
+
+	 // @Test(expected = IllegalArgumentException.class)
+	  public void testAdd_negativeNumbers_shouldThrowException() {
+	    classUnderTest.add(-3, -4);
+	  }
+
+	  //@Test
+	  public void testSubstract() {
+	    assertEquals("substract", 2, classUnderTest.subtract(5, 3));
+	  }
+
+	  //@Test
+	  public void testMultiply() {
+	    assertEquals("multiply", 56, classUnderTest.multiply(7, 8));
+	  }
+
+
+}
+
